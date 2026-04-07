15265c0ae219888c7a265b4d2c4cd587cf3b489b
Stephen Newman <steve@newman.digital>
2026-04-07 19:39:45 +0100

    feat: Add standard sorter harness.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+trait Sorter {
+    fn sort<T>(slice: &mut [T]) where T: Ord;
 }
+
+fn sort<T, S>(slice: &mut [T]) where T: Ord, S: Sorter {
+    S::sort(slice)
+}
+
+// mod bubblesort;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct StdSorter;
+    
+    impl Sorter for StdSorter {
+        fn sort<T>(slice: &mut [T]) where T: Ord {
+            slice.sort()
+        }
+    }
+    
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn std_works() {
+        let mut things = vec![4, 2, 3, 1];
+        sort::<_, StdSorter>(&mut things);
+        assert_eq!(things, &[1, 2, 3, 4]);
     }
 }
